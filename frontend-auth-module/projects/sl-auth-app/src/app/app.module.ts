ffd30cb7f89fef1e8919e89c9274de7e84a1f71c
Sumsum1231 <sumitdash1971@gmail.com>
2024-02-26 09:01:02 +0530

    Removed unecessary files

--- frontend-auth-module/projects/sl-auth-app/src/app/app.module.ts
+++ frontend-auth-module/projects/sl-auth-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlAuthLibraryModule } from 'sl-auth-library';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlAuthLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
