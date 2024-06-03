5b427c70c0c88e30e473e3a0cc4312b43c3cb9be
Syed Ameen <syedameenabrar786@gmail.com>
2024-06-03 15:45:06 +0530

    generalizing verify and generate otp code

--- frontend-auth-module/projects/sl-auth-lib/src/public-api.ts
+++ frontend-auth-module/projects/sl-auth-lib/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of sl-auth-lib
  */
 
-export * from './lib/sl-auth-lib.service';
+// export * from './lib/sl-auth-lib.service';
 export * from './lib/sl-auth-lib.component';
 export * from './lib/sl-auth-lib.module';
 export * from './lib/services/guard/auth.guard';
