b10e9c7aef450e7bf5343057f008e18ef0aa4186
Syed Ameen <syedameenabrar786@gmail.com>
2024-09-30 15:35:53 +0530

    navigate to login page on logout

--- frontend-auth-module/projects/sl-auth-lib/src/lib/services/auth.service.ts
+++ frontend-auth-module/projects/sl-auth-lib/src/lib/services/auth.service.ts
@@ -73,7 +73,7 @@ export class AuthService {
               if (res?.responseCode === "OK") {
                 localStorage.clear();
                 this.sendMessage();
-                this.router.navigate(['/landing']);
+                this.router.navigate(['/login']);
               } else {
                 this.toastService.showToast(res?.message || `Logout unsuccessful`, 'error', 3000, 'top', 'end');
               }
